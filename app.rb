b052ce2f2fcf4357538ed9a10d5c9b987e2d9dc6
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-07-11 14:15:55 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
 	end
+
+	get "/info" do
+		erb :info
+	end
+
+	# get '/' do
+	# 	"Hello World"
+	# end
 end
